548a43a5a7f77e1ee0af538fa9b8e1b99b0a8bb8
yan-2 <yan.zhuang@mail.polimi.it>
2024-05-01 16:43:57 +0200

    fix: env can't be read via vercel

--- server/utility.js
+++ server/utility.js
@@ -1,6 +1,6 @@
 
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = process.env.SUPABASE_URL
-const supabaseKey = process.env.SUPABASE_KEY
-const DB = createClient(supabaseUrl, supabaseKey);
+const SUPABASE_URL="https://gukyiyhhbgrjludizend.supabase.co"
+const SUPABASE_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd1a3lpeWhoYmdyamx1ZGl6ZW5kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQzMDAwMDEsImV4cCI6MjAyOTg3NjAwMX0.-jwS6Ky2z0a24j-aF9rVejNFO2zqu6ApjPSs9mrvoEQ"
+const DB = createClient(SUPABASE_URL, SUPABASE_KEY);
 export {DB}
